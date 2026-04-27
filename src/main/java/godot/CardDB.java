9d3ad503e4b3c01aa019eb1f2965a85cade5ba8b
Nolan <nolanfrost2512@gmail.com>
2026-04-27 16:08:42 +0200

    DB Finished + Label Update
    On peux récupérer les infos des cartes depuis la DB, update du Label pour qu'il affiche atk défense presque finit, nécessite un léger débug.

--- src/main/java/godot/CardDB.java
+++ src/main/java/godot/CardDB.java
@@ -4,7 +4,9 @@ import godot.annotation.Export;
 import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
 import godot.annotation.RegisterProperty;
+import godot.api.DirAccess;
 import godot.api.Node;
+import godot.api.ResourceLoader;
 import godot.global.GD;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,21 +16,53 @@ import java.util.*;
 
 @RegisterClass
 public class CardDB extends Node {
-	@Export
-	public ArrayList<CardData> cardList = new ArrayList<>();
+
 	public HashMap<String, CardData> cards = new HashMap<String, CardData>();
 
 	@Override
 	@RegisterFunction
 	public void _ready(){
 
-		for (CardData card : cardList){
-			if (card != null && card.id != null){
-				cards.put(card.id, card);
+		GD.INSTANCE.print("------LOADING CARD IN DB------");
+		DirAccess dir = DirAccess.open("res://src/main/resources/Cards_Data");
+
+		if (dir != null){
+			dir.listDirBegin();
+
+			String file = dir.getNext();
+
+			while(!file.isEmpty()){
+				if (file.endsWith(".tres")){
+					CardData card = (CardData) ResourceLoader.load("res://src/main/resources/Cards_Data/" + file);
+
+					if (card != null && card.id != null){
+						GD.INSTANCE.print(
+								"LOADED : " + card.id +
+								" ATK : " + card.atk +
+								" DEF : " + card.defense
+						);
+						cards.put(card.id, card);
+					}
+					else {
+						GD.INSTANCE.print("NULL CARD DETECTED");
+					}
+				}
+				file = dir.getNext();
 			}
+			dir.listDirEnd();
+
+			GD.INSTANCE.print("TOTAL CARDS LOADED : " + cards.size());
 		}
+	}
 
 
+	@RegisterFunction
+	public CardData getCard(String cardId){
+		CardData c = cards.get(cardId);
+
+		GD.INSTANCE.print("Requested card : " + cardId);
+
+		return c;
 	}
 
 
--- src/main/java/godot/CardLabel.java
+++ src/main/java/godot/CardLabel.java
@@ -6,6 +6,7 @@ import godot.annotation.RegisterFunction;
 import godot.annotation.RegisterProperty;
 import godot.api.Label;
 import godot.core.Color;
+import godot.global.GD;
 
 @RegisterClass
 public class CardLabel extends Label {
@@ -14,13 +15,23 @@ public class CardLabel extends Label {
 	@Override
 	public void _ready() {
 
-		// Adapter pour aller chercher ATk/DEF dans le dictionnaire/BDD ou toutes les cartes sont présentes.
-		setText("ATK :DEF : ");
-		if (getParent() instanceof Card card){
-			if ("MagicienSombre".equals(card.getCardID())){
-				setText("Magicien Sombre");
-			}
-		}
-
 	}
+
+	public void updateFromCard(Card thisCard){
+
+		thisCard = (Card) this.getParent();
+
+		// Adapter pour aller chercher ATk/DEF dans le dictionnaire/BDD ou toutes les cartes sont présentes.
+		if (thisCard != null){
+
+			setText("ATK : " + thisCard.atk + " DEF : " + thisCard.defense);
+
+			GD.INSTANCE.print("ATK AND DEF LOADED IN LABEL : " + thisCard.atk  + " " + thisCard.defense + " FROM : " + thisCard.cardID);
+
+		}
+		else {
+			GD.INSTANCE.print("thisCard is null");
+		}
+	}
+
 } 
