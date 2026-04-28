aa84bfe8eba9b24d4559e2acafeb7ef05ee17b1d
Nolan <nolanfrost2512@gmail.com>
2026-04-28 17:30:48 +0200

    All Cards Node created + potential fix for Label
    Tous les labels sont sur la même couche, besoin d'un fix, ça créer des conflits d'affichage. Les nodes ont été créer pour toutes les cartes et leur ID est définit (label et sprite se mettent automatiquement).

--- src/main/java/godot/Card.java
+++ src/main/java/godot/Card.java
@@ -45,7 +45,7 @@ public class Card extends Node2D {
 
 			GD.INSTANCE.print("ATK and DEF LOADED : " + atk + " " + defense);
 
-			updateLabel();
+			updateLabel();		// Update Label et Sprite pour qu'ils s'affichent après l'exécution du script de la carte
 			updateSprite();
 		}
 		else {
@@ -88,12 +88,14 @@ public class Card extends Node2D {
 	public void _on_area_2d_mouse_entered() {
 		setScale(new Vector2(1.05, 1.05));
 		this.setZIndex(2);
+		updateLabel();
 	}
 
 	@RegisterFunction
 	public void _on_area_2d_mouse_exited() {
 		setScale(new Vector2(1, 1));
 		this.setZIndex(1);
+		updateLabel();
 	}
 
 }
