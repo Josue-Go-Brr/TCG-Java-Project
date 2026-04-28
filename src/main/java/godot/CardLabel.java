ad1fccb743468126f1a268b05599c61d9cd49339
Nolan <nolanfrost2512@gmail.com>
2026-04-28 17:35:24 +0200

    Test Push

--- src/main/java/godot/CardLabel.java
+++ src/main/java/godot/CardLabel.java
@@ -23,8 +23,8 @@ public class CardLabel extends Label {
 		if (thisCard != null){
 
 			setText("ATK : " + thisCard.atk + "\nDEF : " + thisCard.defense);
-			//setZIndex(thisCard.getZIndex());
-			// GD.INSTANCE.print("ATK AND DEF LOADED IN LABEL : " + thisCard.atk  + " " + thisCard.defense + " FROM : " + thisCard.cardID);
+
+			GD.INSTANCE.print("ATK AND DEF LOADED IN LABEL : " + thisCard.atk  + " " + thisCard.defense + " FROM : " + thisCard.cardID);
 
 		}
 		else {
