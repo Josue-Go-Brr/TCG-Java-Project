d353f0ca3106ba460ea0457cdfa5155b7267942a
doli asma <guedouah.asma27@gmail.com>
2026-05-10 00:39:41 +0200

    bla bla

--- src/main/java/godot/library/CardTileController.java
+++ src/main/java/godot/library/CardTileController.java
@@ -2,6 +2,7 @@ package godot.library;
 
 import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
+import godot.api.FontFile;
 import godot.api.InputEvent;
 import godot.api.InputEventMouseButton;
 import godot.api.Label;
@@ -16,6 +17,14 @@ import godot.core.MouseButton;
 
 @RegisterClass
 public class CardTileController extends PanelContainer {
+	private static final String FONT_STONE_SERIF = "res://assets/fonts/TCG fonts/Stone Serif Semibold.ttf";
+	private static final String FONT_COST = "res://assets/fonts/TCG fonts/Yu-Gi-Oh! ITC Stone Serif Small Caps Bold.ttf";
+
+	private static final String NODE_CARD_IMAGE = "Margin/Content/CardImage";
+	private static final String NODE_OVERLAY_STATS = "Margin/Content/CardImage/Overlay/CardStats";
+	private static final String NODE_OVERLAY_COST = "Margin/Content/CardImage/Overlay/CardCostValue";
+	private static final String NODE_OVERLAY_NAME = "Margin/Content/CardImage/Overlay/CardName";
+
 	private TextureRect cardImageNode;
 	private Label cardNameNode;
 	private Label cardStatsNode;
@@ -26,13 +35,12 @@ public class CardTileController extends PanelContainer {
 	@RegisterFunction
 	@Override
 	public void _ready() {
-		cardImageNode = (TextureRect) getNodeOrNull("Margin/Content/CardImage");
-		cardStatsNode = (Label) getNodeOrNull("Margin/Content/CardImage/Overlay/CardStats");
-		cardCostValueNode = (Label) getNodeOrNull("Margin/Content/CardImage/Overlay/CardCostValue");
-		cardNameNode = (Label) getNodeOrNull("Margin/Content/CardName");
+		resolveTileNodes();
 		ensureOverlayLabels();
+		if (cardData != null) {
+			applyOverlayValues(cardData);
+		}
 
-		
 		setMouseFilter(Control.MouseFilter.PASS);
 		if (cardImageNode != null) {
 			cardImageNode.setMouseFilter(Control.MouseFilter.IGNORE);
@@ -50,39 +58,64 @@ public class CardTileController extends PanelContainer {
 
 	public void setCardData(BaseCarte card) {
 		cardData = card;
-		if (cardStatsNode == null || cardCostValueNode == null) {
-			ensureOverlayLabels();
-		}
+		resolveTileNodes();
+		ensureOverlayLabels();
 		if (cardNameNode != null) {
 			cardNameNode.setText(card.getName());
 		}
 		if (cardImageNode != null) {
 			cardImageNode.setTexture(resolveTexture(card));
 		}
+		applyOverlayValues(card);
+		setTooltipText(card.getName());
+	}
+
+	private void resolveTileNodes() {
+		if (cardImageNode == null) {
+			cardImageNode = (TextureRect) getNodeOrNull(NODE_CARD_IMAGE);
+		}
+		if (cardStatsNode == null) {
+			cardStatsNode = (Label) getNodeOrNull(NODE_OVERLAY_STATS);
+		}
+		if (cardCostValueNode == null) {
+			cardCostValueNode = (Label) getNodeOrNull(NODE_OVERLAY_COST);
+		}
+		if (cardNameNode == null) {
+			cardNameNode = (Label) getNodeOrNull(NODE_OVERLAY_NAME);
+			if (cardNameNode == null) {
+				cardNameNode = (Label) getNodeOrNull("Margin/Content/CardName");
+			}
+		}
+	}
+
+	private void applyOverlayValues(BaseCarte card) {
+		if (card == null) {
+			return;
+		}
 		if (cardStatsNode != null) {
+			cardStatsNode.setVisible(true);
 			cardStatsNode.setText(buildStatsText(card));
 		}
 		if (cardCostValueNode != null) {
+			cardCostValueNode.setVisible(true);
 			cardCostValueNode.setText(String.valueOf(card.getCost()));
 		}
-		setTooltipText(card.getName());
 	}
 
 	private String buildStatsText(BaseCarte card) {
 		if (card instanceof CarteMonster monster) {
-			return "ATK: " + monster.getAttack() + "\nDEF: " + monster.getDefense();
+			return "ATK : " + monster.getAttack() + "\nDEF : " + monster.getDefense();
 		}
-		return "ATK: -\nDEF: -";
+		return "ATK : -\nDEF : -";
 	}
 
 	private void ensureOverlayLabels() {
-		cardStatsNode = (Label) getNodeOrNull("Margin/Content/CardImage/Overlay/CardStats");
-		cardCostValueNode = (Label) getNodeOrNull("Margin/Content/CardImage/Overlay/CardCostValue");
+		resolveTileNodes();
 		if (cardStatsNode != null && cardCostValueNode != null) {
 			return;
 		}
 		if (cardImageNode == null) {
-			cardImageNode = (TextureRect) getNodeOrNull("Margin/Content/CardImage");
+			cardImageNode = (TextureRect) getNodeOrNull(NODE_CARD_IMAGE);
 		}
 		if (cardImageNode == null) {
 			return;
@@ -106,10 +139,10 @@ public class CardTileController extends PanelContainer {
 		if (cardStatsNode == null) {
 			cardStatsNode = new Label();
 			cardStatsNode.setName("CardStats");
-			cardStatsNode.setPosition(new godot.core.Vector2(14, 260));
-			cardStatsNode.setCustomMinimumSize(new godot.core.Vector2(156, 40));
-			cardStatsNode.set("theme_override_font_sizes/font_size", 15);
-			cardStatsNode.setText("ATK: -\nDEF: -");
+			cardStatsNode.setPosition(new godot.core.Vector2(24, 271));
+			cardStatsNode.setCustomMinimumSize(new godot.core.Vector2(156, 47));
+			applyAtkDefLabelTheme(cardStatsNode);
+			cardStatsNode.setText("ATK : -\nDEF : -");
 			cardStatsNode.setMouseFilter(Control.MouseFilter.IGNORE);
 			overlay.addChild(cardStatsNode);
 		}
@@ -118,15 +151,43 @@ public class CardTileController extends PanelContainer {
 		if (cardCostValueNode == null) {
 			cardCostValueNode = new Label();
 			cardCostValueNode.setName("CardCostValue");
-			cardCostValueNode.setPosition(new godot.core.Vector2(186, 268));
+			cardCostValueNode.setPosition(new godot.core.Vector2(178, 278));
 			cardCostValueNode.setCustomMinimumSize(new godot.core.Vector2(40, 32));
 			cardCostValueNode.set("horizontal_alignment", 1);
 			cardCostValueNode.set("vertical_alignment", 1);
-			cardCostValueNode.set("theme_override_font_sizes/font_size", 24);
+			applyCostLabelTheme(cardCostValueNode);
 			cardCostValueNode.setText("-");
 			cardCostValueNode.setMouseFilter(Control.MouseFilter.IGNORE);
 			overlay.addChild(cardCostValueNode);
 		}
+	}
+
+	private FontFile loadFontFile(String path) {
+		Object loaded = ResourceLoader.load(path, "FontFile", ResourceLoader.CacheMode.REUSE);
+		return loaded instanceof FontFile ? (FontFile) loaded : null;
+	}
+
+	private void applyAtkDefLabelTheme(Label label) {
+		label.set("theme_override_colors/font_color", new godot.core.Color(0, 0, 0, 1));
+		FontFile font = loadFontFile(FONT_STONE_SERIF);
+		if (font != null) {
+			label.set("theme_override_fonts/font", font);
+		}
+		label.set("theme_override_font_sizes/font_size", 18);
+	}
+
+	private void applyCostLabelTheme(Label label) {
+		label.set("theme_override_colors/font_color", new godot.core.Color(0.618854f, 0.748261f, 0.952736f, 1));
+		label.set("theme_override_colors/font_shadow_color", new godot.core.Color(0, 0, 0, 1));
+		label.set("theme_override_colors/font_outline_color", new godot.core.Color(0, 0, 0, 1));
+		label.set("theme_override_constants/shadow_offset_x", 4);
+		label.set("theme_override_constants/shadow_offset_y", 2);
+		label.set("theme_override_constants/outline_size", 10);
+		FontFile font = loadFontFile(FONT_COST);
+		if (font != null) {
+			label.set("theme_override_fonts/font", font);
+		}
+		label.set("theme_override_font_sizes/font_size", 25);
 	}
 
 	public BaseCarte getCardData() {
